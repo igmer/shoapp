e1bac28941433a97dd855c989220e1d585c4db52
rodriguez <igmerdz@gmail.com>
2018-10-26 15:38:47 -0600

    se creara pedido

--- app/src/main/java/com/example/edwin/shopapp/DetalleProductoActivity.java
+++ app/src/main/java/com/example/edwin/shopapp/DetalleProductoActivity.java
@@ -2,13 +2,24 @@ package com.example.edwin.shopapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class DetalleProductoActivity extends AppCompatActivity {
     String codigo,url;
     ImageView imDetalle;
+    Button btnAdd;
+    static ArrayList pedido=new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +29,34 @@ public class DetalleProductoActivity extends AppCompatActivity {
         codigo = bundle.getString("codigo");
         url = bundle.getString("url");
         imDetalle =(ImageView)findViewById(R.id.imDetalleProducto);
-
         Picasso.get().load(url).into(imDetalle);
+        btnAdd = (Button)findViewById(R.id.btnConfirmar);
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),codigo,Toast.LENGTH_LONG).show();
+                pedido.add(codigo);
+            }
+        });
+
+    }
+    public Integer addPedido(String codigoProd){
+        Connection connect; connect = ConexionSQL.ConnectionHelper();
+        String mensaje_error="";
+        Integer existe=0;
+        try {
+            Statement st = connect.createStatement();
+            String comando="INSERT INTO PRODUCTO";
+            ResultSet rs = st.executeQuery(comando);
+            while (rs.next()) {
+                existe++;
+            }
+            connect.close();
+        } catch (Exception e) {
+            Log.i("Error**",e.getMessage());
+            existe=0;
+        }
+        return existe;
     }
 }
--- app/src/main/java/com/example/edwin/shopapp/Principal.java
+++ app/src/main/java/com/example/edwin/shopapp/Principal.java
@@ -3,6 +3,7 @@ package com.example.edwin.shopapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.view.Menu;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -36,11 +38,12 @@ public class Principal extends AppCompatActivity {
 
     Button btnIngresar, btnSalir;
     //EditText edtEmail, edtClave;
-    TextInputEditText edtEmail, edtClave;
+   static TextInputEditText edtEmail, edtClave;
     CardView cv_loggin;
     CardView cv_registro;
-    Switch swRemember;
-    SharedPreferences prefs;
+    static Switch swRemember;
+    ProgressBar pbLogin;
+    static SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +54,7 @@ public class Principal extends AppCompatActivity {
         edtClave=(TextInputEditText) findViewById(R.id.titPass);
         CardView card_view = (CardView) findViewById(R.id.cardView);
         CardView cv_registrar = (CardView) findViewById(R.id.cv_registro);
+        pbLogin = (ProgressBar)findViewById(R.id.pbLogin);
         swRemember = (Switch)findViewById(R.id.swRemember);
         prefs = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
 
@@ -63,15 +67,16 @@ public class Principal extends AppCompatActivity {
                 }else if(edtClave.getText().toString().trim().length()==0){
                     edtClave.setError("Dato Requerido para Ingresar");
                 }else{
-                    if(validaIngresousuario(edtEmail.getText().toString().trim(),edtClave.getText().toString().trim())>=1){
+                    checkinServer check = new checkinServer();
+                    check.execute("");
+                    /*if(validaIngresousuario(edtEmail.getText().toString().trim(),edtClave.getText().toString().trim())>=1){
                         saveOnPreferences(edtEmail.getText().toString().trim(),edtClave.getText().toString().trim());
                         Intent i = new Intent(getApplicationContext(),MenusTabs.class);
                         startActivity(i);
                         finish();
+                        */
 
-                }else{
-                        Toast.makeText(getApplicationContext(),"No encontrado",Toast.LENGTH_LONG).show();
-                    }
+
                 }
 
 
@@ -89,38 +94,81 @@ public class Principal extends AppCompatActivity {
 
 
     }
+    public class checkinServer extends AsyncTask<String,String,Integer>{
 
-
-
-
-    public Integer validaIngresousuario( String elUsuario,  String laClave){
-        Connection connect; connect = ConexionSQL.ConnectionHelper();
-        String mensaje_error="";
-        Integer existe=0;
-        try {
-            Statement st = connect.createStatement();
-            String comando="SELECT usuario, password FROM usuarios where usuario='" +  elUsuario + "'";
-            comando+=" and password='" + laClave+"'";
-            ResultSet rs = st.executeQuery(comando);
-            while (rs.next()) {
-                existe++;
+        @Override
+        protected Integer doInBackground(String... strings) {
+            String elUsuario= edtEmail.getText().toString().trim();
+            String laClave =  edtClave.getText().toString().trim();
+            Connection connect; connect = ConexionSQL.ConnectionHelper();
+            String mensaje_error="";
+            Integer existe=0;
+            try {
+                Statement st = connect.createStatement();
+                String comando="SELECT usuario, password FROM usuarios where usuario='" +  elUsuario + "'";
+                comando+=" and password='" + laClave+"'";
+                ResultSet rs = st.executeQuery(comando);
+                while (rs.next()) {
+                    existe++;
+                }
+                connect.close();
+            } catch (Exception e) {
+                Log.i("Error**",e.getMessage());
+                existe=0;
+               // return false;
             }
-            connect.close();
-        } catch (Exception e) {
-            Log.i("Error**",e.getMessage());
-            existe=0;
+            //return existe;
+            //   }
+
+            return existe;
         }
-        return existe;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pbLogin.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected void onPostExecute(Integer existe) {
+            //super.onPostExecute(existe);
+            if (existe>0){
+                saveOnPreferences(edtEmail.getText().toString().trim(),edtClave.getText().toString().trim());
+                Intent i = new Intent(getApplicationContext(),MenusTabs.class);
+                startActivity(i);
+                finish();
+            }else{
+                edtClave.setError("Credenciales no validas");
+                edtEmail.setError("Credenciales no validas");
+                pbLogin.setVisibility(View.INVISIBLE);
+            }
+        }
+
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
     }
 
+
+
+
+
+
     /** metodo para guardar en Shared preferences las credenciales de usuario**/
-    private void saveOnPreferences(String user, String pass) {
+    private static void saveOnPreferences(String user, String pass) {
         if (swRemember.isChecked()) {
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("user", user);
             editor.putString("pass", pass);
+            editor.putString("usuarioBD", pass);
             editor.apply();
-
+        }else{
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("usuarioBD", user); // este lo usaremos para el usuario que se enviare ellos pedidos
+            editor.apply();
         }
     }
 
